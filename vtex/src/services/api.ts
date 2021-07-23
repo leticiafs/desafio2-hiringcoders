8e5d713916c40a3a2cc0cad20f267752b6cbeaca
Leticia Felix <53658438+leticiafs@users.noreply.github.com>
2021-07-23 01:33:22 -0300

    Update api.ts

--- vtex/src/services/api.ts
+++ vtex/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'http://localhost:8888/produtos'
+  baseURL: 'https://my-json-server.typicode.com/leticiafs/desafio2-hiringcoders/produtos'
 })
 
 
